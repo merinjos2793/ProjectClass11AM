05bdc89da0499c7e39bf1b1cef77413108d5e869
merinjos2793 <merinraj2708@gmail.com>
2024-04-11 15:17:16 +0530

    Added methods in Class A and Created Class B

---
+++ src/test/java/com/gitclass/ClassB.java
@@ -0,0 +1,15 @@
+package com.gitclass;
+
+public class ClassB {
+
+	private void java() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void selenium() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
